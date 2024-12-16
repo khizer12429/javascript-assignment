77cfce990ad99d19831e029a9b0a5b276201798a
khizer12429 <khizerfarooq267@gmail.com>
2024-12-16 15:39:35 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,81 @@
+// pdf1
+// question no 1
+// alert("welcome ; hello");
+
+// question no 2
+// alert("Error! Please enter a valid password");
+
+// question no 3
+// alert("Welcome to js land..\nHappy codding");
+
+
+// question no 4
+// alert("Welcome to js land..");
+// alert("Happy codding")
+
+// question no 5
+// alert("Hello, I can js through my web browser's console");
+
+// question no 6
+// alert("alert is use to display any thing that might be usefull");
+
+
+// pdf2
+// question no 1
+// var username;
+
+// question no 2
+// var myname="khizer farooq";
+
+// question no 3
+// message="hello world";
+// alert(message);
+
+// question no 4
+// var username="jhone doe";
+// var age= 20;
+// var certificate="Certified Mobile Application Development";
+// alert(username);
+// alert(age);
+// alert(certificate);
+
+// question no 5
+// var username="pizza\npizz\npiz\npi\np";
+// alert(username);
+
+// question no 6
+// var email="khizerfarooq267@gmail.com";
+// alert("my email address is " + email);
+
+// question no 7
+// var books = "A smarter way to learn JavaScript";
+// alert("i am trying to learn from book " + books);
+
+
+// question no 8 
+// var a = "Yeh!I can write html content through javascript";
+// document.write(a);
+
+// question no 9
+// let b="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(b);
+
+
+// pdf3
+// question no 1
+// var age=20;
+// alert("I am " + age + " year old");
+
+// question no 2
+// var c=15;
+// alert("You have visited this site " + c + " times");
+
+// question no 3
+// var birthyear = 2003;
+// document.write("my birth year is "+ birthyear );
+// document.write(" data type of declared variable is number");
+
+// question no 4
+// var visitorname="john doe";
+// var producttittle="5 T-shirts"
+// document.write(visitorname + "ordered "+ producttittle+" on XYZ Clothing store");
